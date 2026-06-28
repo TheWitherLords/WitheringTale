d3f126c1ec0e105401aca5a585af7121dfdc3c4c
TheWitherLords <yannick.franke01@gmx.de>
2026-06-28 06:59:37 +0200

    Main File Correction

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-local jokers_src = SMODS.NFS.getDirectoryItems(SMODS.current:mod.path .. "src/jokers")
+local jokers_src = SMODS.NFS.getDirectoryItems(SMODS.current_mod.path .. "src/jokers")
 for _, file in ipairs(jokers_src) do
     assert(SMODS.load_file("jokers/" ..file))()
 end
